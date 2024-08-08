604d69fbbb2e520cc2b34a4d564deaa938c4e9d8
John Doe <john.doe@example.com>
2024-08-08 16:20:06 +0530

    new login and register features are added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import Clone from "./Main";
 import Search from "./Components/Header/Searchitems";
 import Offers from "./Components/Header/Offers";
 import Menu from "./Components/Main/Container-3/Menu/Menu";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Components/Accounts/UserLogin";
+import Register from "./Components/Accounts/UserRegister";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  const handleLogin = (authStatus) => {
+    setIsAuthenticated(authStatus);
+  };
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Clone />} />
+          <Route
+            path="/dashboard"
+            element={isAuthenticated ? <Clone /> : <Navigate to="/dashboard" />}
+          />
           <Route path="/search" element={<Search />} />
+          <Route
+            path="/"
+            element={<Login onLogin={handleLogin} />}
+          />
+          <Route path="/register" element={<Register />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/menu/:id" element={<Menu />} />
         </Routes>
