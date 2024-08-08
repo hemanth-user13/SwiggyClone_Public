22bb2d1e9bb544b785b3c36a602020c4e3138886
22210301016 <bolgamhemanth7878@gmail.com>
2024-08-08 08:25:46 +0530

    the new id features are added

--- src/Components/Main/Container-3/Cards.js
+++ src/Components/Main/Container-3/Cards.js
@@ -1,34 +1,45 @@
-import React from 'react'
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Cards({ resdata, error, onClick }) {
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 pt-20">
-            {resdata.map((item) => (
-                <div key={item.id} className="transform transition-transform duration-300 hover:scale-105" onClick={() => onClick(item.id)}>
-                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 relative overflow-hidden">
-                        <a href="#">
-                            <div className="relative">
-                                <img className="w-full h-48 object-cover rounded-t-lg" src={item.img_url} alt={item.img_title} />
-                                <div className="absolute top-0 right-0 bg-red-600 text-white text-sm font-bold py-1 px-2 rounded-bl-lg">
-                                    {item.discount}
-                                </div>
-                            </div>
-                        </a>
-                        <div className="p-5">
-                            <a href="#">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.img_title}</h5>
-                            </a>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                            </p>
-                        </div>
-                    </div>
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 pt-20">
+      {resdata.map((item) => (
+        <div
+          key={item.id}
+          className="transform transition-transform duration-300 hover:scale-105"
+          onClick={() => onClick(item.id)}
+        >
+          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 relative overflow-hidden">
+            <a href="">
+              <div className="relative">
+                <img
+                  className="w-full h-48 object-cover rounded-t-lg"
+                  src={item.img_url}
+                  alt={item.img_title}
+                />
+                <div className="absolute top-0 right-0 bg-red-600 text-white text-sm font-bold py-1 px-2 rounded-bl-lg">
+                  {item.discount}
                 </div>
-            ))}
+              </div>
+            </a>
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {item.img_title}
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
--- src/Components/Main/Container-3/Menu/Menu.js
+++ src/Components/Main/Container-3/Menu/Menu.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useParams, useLocation } from "react-router-dom";
+import Navbar from "../../../Header/NavBar";
 
 function Menu() {
+  const { id } = useParams();
+  const location = useLocation();
+  const { img_title } = location.state || {};
+  // const { food_items } = location.state || {};
+
   return (
     <div>
-      <h1>
-        hi these is a menu section
-      </h1>
+      <Navbar />
+      <h1>Hi, this is a menu section</h1>
+      <p>Selected Item ID: {id}</p>
+      <p>Selected Item Title: {img_title}</p>
+      {/* <p>Selected food items are {food_items}</p> */}
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
