fd3c945845cabaca04a154d69a36876d184c1f66
22210301016 <bolgamhemanth7878@gmail.com>
2024-08-10 13:15:50 +0530

    the bug is fixed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Clone from './Main'; 
-import Search from './Components/Header/Searchitems'; 
-import Offers from './Components/Header/Offers';
-import Menu from './Components/Main/Container-3/Menu/Menu'; 
-import UserLogin from './Components/Accounts/UserLogin';
-import UserRegister from './Components/Accounts/test';
-import PageNotFound from './Components/Error/Error';
-import DashboardSign from './Components/Accounts/Login'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Clone from "./Main";
+import Search from "./Components/Header/Searchitems";
+import Offers from "./Components/Header/Offers";
+import Menu from "./Components/Main/Container-3/Menu/Menu";
+import UserLogin from "./Components/Accounts/UserLogin";
+import UserRegister from "./Components/Accounts/UserRegister";
+import PageNotFound from "./Components/Error/Error";
+import DashboardSign from "./Components/Accounts/Login";
 
 function App() {
-    const handleLogin = (isLoggedIn) => {
-        console.log("User login status:", isLoggedIn);
-    };
+  const handleLogin = (isLoggedIn) => {
+    console.log("User login status:", isLoggedIn);
+  };
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/dashboard/:id" element={<Clone />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/offers" element={<Offers />} />
-                <Route path="/menu/:id" element={<Menu />} />
-                <Route Path="/sign" element={<DashboardSign/>}/>
-                <Route path="/" element={<UserLogin onLogin={handleLogin} />} />
-                <Route path="/register" element={<UserRegister />} />
-                <Route path='*' element={<PageNotFound/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard/:id" element={<Clone />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/menu/:id" element={<Menu />} />
+        <Route Path="/sign" element={<DashboardSign />} />
+        <Route path="/" element={<UserLogin onLogin={handleLogin} />} />
+        <Route path="/register" element={<UserRegister />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
